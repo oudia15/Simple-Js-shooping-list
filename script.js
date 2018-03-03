52228b961419b1762be2d4392efbc71122c4c1bb
oudia15@hotmail.fr <oudia15@hotmail.fr>
2018-03-02 21:04:13 -0600

    Ability to delete or mark as done added

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var input = document.querySelector('#input');
 var ul = document.querySelector('ul');
 var h1 = document.querySelector('h1');
 var alert = document.querySelector('#alert');
+var lists = document.querySelectorAll('li');
+var deleteBtns = document.querySelectorAll('.delete');
 
 btn.addEventListener("click", handleSubmit);
 
@@ -12,10 +14,28 @@ input.addEventListener("keypress", (event) => {
   }
 });
 
+lists.forEach((li) => {
+  li.addEventListener("click", () => {
+      li.classList.toggle('done');
+  });
+});
+
+deleteBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+      var li = btn.parentElement;
+      li.remove();
+  });
+});
+
+
 function handleSubmit() {
   if(input.value) {
     var li = document.createElement('li');
+    var deletebtn = document.createElement('button');
+    deletebtn.className = "delete";
+    deletebtn.innerText = "X"
     li.innerText = input.value;
+    li.append(deletebtn);
     ul.prepend(li);
     alert.className = 'green';
     alert.innerText = "Element added";
@@ -23,6 +43,13 @@ function handleSubmit() {
     setTimeout(()=> {
       alert.innerText= "";
     }, 2000);
+    deletebtn.addEventListener("click", () => {
+        var li = deletebtn.parentElement;
+        li.remove();
+    });
+    li.addEventListener("click", () => {
+        li.classList.toggle('done');
+    });
   } else {
     alert.className= 'reds';
     alert.innerText = "You must enter an input";
